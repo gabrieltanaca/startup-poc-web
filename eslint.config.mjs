fd2cf7e8616d3a01a8a7b642f2d8e9aaf54b13a3
Gabriel Tanaca <gabrieltanacaramos@gmail.com>
2025-11-19 21:32:02 -0300

    chore: Add initial project configuration and UI components
    - Created .editorconfig and .prettierrc for code formatting.
    - Added .env.example for environment variable setup.
    - Introduced ESLint configuration with custom rules.
    - Added Tailwind CSS configuration and utility classes.
    - Implemented foundational UI components: Avatar, Badge, Button, Card, Dialog, Dropdown Menu, Form, Input, Label, Separator, Sheet, Sidebar, Skeleton, Table, Tooltip, and Toaster.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,37 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
+  prettierConfig,
+  {
+    rules: {
+        "no-console": ["warn", { "allow": ["warn", "error"] }],
+        "react/no-unescaped-entities": "off",
+        "@next/next/no-img-element": "off",
+        "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+        "prefer-const": "warn",
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error", 
+        "simple-import-sort/imports": [
+            "error",
+            {
+            	// 1.Módulos Node (built-in) & Módulos externos | 2.Alias | 3 & 4.Imports Relativos (../ e ./) | 5.Imports de CSS 
+                groups: [
+					["^\\u0000", "^react", "^next", "^@?\\w"],
+					["^@/"],
+					["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+					["^\\./(?=.*/)(?!.*\\.(css|less|scss|sass)$)", "^\\.(?!/?$)", "^\\./?$"],
+					["^.+\\.(css|less|scss|sass)$"],
+                ],
+            },
+      ],
+    },
+  },
+  globalIgnores([ 
     ".next/**",
     "out/**",
     "build/**",
